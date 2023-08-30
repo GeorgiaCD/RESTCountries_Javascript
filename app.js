96367235b89d7fb11a15eeec6b29cd28bac05b08
GeorgiaCD <georgiacrawforddellar@gmail.com>
2023-08-30 18:45:32 +0100

    added function

--- app.js
+++ app.js
@@ -1,19 +1,54 @@
-const fetchCountries = async () =>{
+const enter = document.querySelector("#enter");
+const filterList = document.querySelector("#filter-list");
+const input = document.querySelector("#countryEntry");
+let countries = [];
+
+const setUp = async () => {
+    fetchData()
+}
+
+const fetchData = async () => {
     const response = await fetch("https://restcountries.com/v3.1/all");
     const jsonData = await response.json();
+    countries = jsonData; 
+    populateList()
+}
 
-    const countryList = document.createElement("ul");
 
-    jsonData.forEach((countryData) => {
+const populateList = async () =>{
+
+    const countryList = document.querySelector("ul");
+
+    countries.forEach((countryData) => {
         const country = document.createElement("li");
-        country.textContent = countryData.name.common;
+        country.textContent = "Name: "+countryData.name.common;
+        const countryPop = document.createElement("li");
+        countryPop.innerText = "Population: "+ countryData.population;
         countryList.appendChild(country);
+        country.appendChild(countryPop);
     });
 
     document.querySelector("body").appendChild(countryList);
 
 }
 
-fetchCountries();
+
+
+setUp();
+// fetchData();
+
+const filterCountry = (country) =>{
+    // grab the form
+    // grab the input
+    // call a filter function (substring in string:the country name)
+    // reassign countries variable
+    // call populate list again 
+    const form = document.querySelector("form");
+    
+
+    
+}
+
+
 
 
